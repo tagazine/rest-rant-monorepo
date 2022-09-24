877f0d905b9d9fb0b9305ffd186bf88f3854754f
Your Namegit config --global user.name Your <you@example.com>
2022-09-24 09:09:19 -0400

    AWS Maybe?

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
 const defineCurrentUser = require('./middleware/defineCurrentUser')
+const path = require('path')
+
 
 // Express Settings
 app.use(cors())
@@ -12,6 +14,11 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(defineCurrentUser)
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+}
+
 
 // Controllers & Routes
 
